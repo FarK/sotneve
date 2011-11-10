16c7f003b4d418c93d64a0c11059d3096588bc93
rafaespillaque@gmail.com <rafaespillaque@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-10 15:12:19 +0000

    Cambiado metodo de notificacion de errores desde php a html. Usando GET

--- login.php
+++ login.php
@@ -18,19 +18,15 @@
 					$_SESSION['idUsuario'] = $row['idUsuario'];
 			}else{
 				//Es incorrecta
-					//Variable error a true para el html
-					$_SESSION['err_pass'] = true;
-					//Redireccionar
-					header('Location:index.php');
+					//Redireccionar con GET a error
+					header('Location:index.php?err_pass');
 			}
 			//Desconectar de la bd
 		$bd->desconectar();
 	}else{
 	//No puedo conectar
-		//Variable error para el html
-		$_SESSION['err_bd'] = true;
-		//Redirecconar
-		header('Location:index.php');
+		//Redirecconar con GET a error
+		header('Location:index.php=err_bd');
 	}
 	
 	echo 'Logueado, tu id es: ' . $_SESSION['idUsuario'] . '<br>';
