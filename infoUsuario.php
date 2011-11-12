5269734f94626b8a184c61fc3e5fd16eb3fabfea
fark.zano@gmail.com <fark.zano@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-12 18:21:09 +0000

    Creada la clase php usuario para consultas. Avanzada infoUsuario.php

--- infoUsuario.php
+++ infoUsuario.php
@@ -4,42 +4,65 @@
 		//header('Location:index.php');
 	}		
 
-	include_once('BD/GestorBD.php');
-	//Iniciar sesion
-	//session_start();
-	//Crear objeto gestor bd
-	$bd = new GestorBD();	
-	//Conectar a la bd
-	if($bd->conectar()){ //Pudo conectar
-		
-		$usuario = $bd->getUsuario($_GET["idUsuario"]);
+	include_once('BD/usuario.php');
 
-		//Si no existe el usuario (o ha fallado la consulta)
-		if(!$usuario)
-			header('Location:errores.php?error="userNotFound"');
-			//echo "error";
+	//Crear objeto usuario
+	$usuario = new Usuario($_GET["idUsuario"]);
+	//Comprobar si ha habido errores
+	if($usuario->error() == -2) //No pudo conectar
+		header('Location:index.php?err_bd');	//Redirecconar con GET a error
+	
+	else if($usuario->error() == -1)//no existe el usuario (o ha fallado la consulta)
+		header('Location:errores.php?error="userNotFound"');
 
-		//Desconectar de la bd
-		$bd->desconectar();
-	}else{
-	//No puedo conectar
-		//Redirecconar con GET a error
-		header('Location:index.php?err_bd');
-	}
 
 ?>
+
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!-- IMPORTANTE ESA LÍNEA DE AHÍ ARRIBA Y LA DE ABAJO!!!  -->
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
 	<head>
 		<!-- IMPORTANTE ESA LÍNEA DE ABAJO!!!  -->
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Sotneve - Login</title>
+		<title>Sotneve - <?php echo $usuario->getCampo("alias"); ?></title>
 		<link rel="stylesheet" type="text/css" href="styles/index.css" />
 		<script type="text/javascript" src="scripts/index.js"></script>
 	</head>
 	<body>
-asd	
-		<h1><?php echo $usuario["alias"]?><h1>
+		<h1><?php echo $usuario->getCampo("alias")?></h1>
+		
+		<div class="info_usuario">
+			<span id="nombre" class="campo_usuario">
+				<?php
+					if($usuario->esVisible($NOMBRE)) echo $usuario->getCampo("nombre");
+				?>
+			</span>
+			<span id="apellidos" class="campo_usuario">
+				<?php
+					if($usuario->esVisible($APELLIDOS)) echo $usuario->getCampo("apellidos");
+				?>
+			</span>
+			<span id="sexo" class="campo_usuario">
+				<?php
+					if($usuario->esVisible($SEXO)) echo $usuario->getCampo("sexo");
+				?>
+			</span>
+			<span id="fechaNac" class="campo_usuario">
+				<?php
+					if($usuario->esVisible($FECHA_NAC)) echo $usuario->getCampo("fechaNac");
+				?>
+			</span>
+			<span id="email" class="campo_usuario">
+				<?php
+					if($usuario->esVisible($EMAIL)) echo $usuario->getCampo("email");
+				?>
+			</span>
+			<span id="provincia" class="campo_usuario">
+				<?php
+					echo $usuario->getCampo("provincia");
+				?>
+			</span>
+		</div>
 	</body>
 </html>
