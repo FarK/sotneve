ebcb70447f3aaa89ac8b19bcb0c3062581ec83a9
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-12 18:01:11 +0000

    favoritos.php ya lista los favoritos de un id y tiene su hoja de estilo

--- trunk/favoritos.php
+++ trunk/favoritos.php
@@ -1,26 +1,45 @@
 <HTML>
 	<head>
+		<link rel="stylesheet" type="text/css" href="styles/favoritos.css" />
 		<title>Tus favoritos</title>
+		
 	</head>
 	<body>
-		
-<?php
+		<div id=listafavoritos>
+		<h1>Estos son tus favoritos!</h1>
+		<?php
 
-include_once ('BD/GestorBD.php');
+		include_once ('BD/GestorBD.php');
 
-session_start();
-if(!$_SESSION['logged'])
-	header('Location:index.php');
+		// session_start();
+		// if(!$_SESSION['logged'])
+		// header('Location:index.php');
 
-$bd = new GestorBD();
+		$bd = new GestorBD();
 
-if ($bd -> conectar()) {
+		$idusuarioactual = 4;
+		//POner un Id que exista
 
+		if ($bd -> conectar()) {
 
+			$query = sprintf("SELECT * FROM favoritos WHERE idUsuario1= '%s'", $idusuarioactual);
+			$tuplas = $bd -> consulta($query);
+			while ($fila = mysql_fetch_assoc($tuplas)) {
+				$idUsuario2 = $fila['idUsuario2'];
 
-}
+				$query = sprintf("SELECT alias FROM usuarios WHERE idUsuario= '%s'", $idUsuario2);
+				$aux = $bd -> consulta($query);
+				
+				
+				while ($fila = mysql_fetch_assoc($aux)) {
+					$alias = $fila['alias'];
+					$span= sprintf("<span><a href='infoUsuario.php?idUsuario=%s'>%s</a></span>\n\t\t", $idUsuario2,$alias);
+					echo $span;
+				}
+			}
 
-?>
-		
+		}
+	?>
+	</div>
 	</body>
 </HTML>
