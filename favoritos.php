508748de95afb555797abb837363163e47e21325
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-12 18:15:07 +0000

    modificado favoritos.css

--- favoritos.php
+++ favoritos.php
@@ -1,4 +1,6 @@
-<HTML>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 	<head>
 		<link rel="stylesheet" type="text/css" href="styles/favoritos.css" />
 		<title>Tus favoritos</title>
@@ -42,4 +44,4 @@
 	?>
 	</div>
 	</body>
-</HTML>
+</html>
