401a1d3ce6f91dd31abb817bfef9d0df6047de0b
fark.zano@gmail.com <fark.zano@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-13 00:24:25 +0000

    Adaptado favoritos.php a la nueva clase Usuario

--- favoritos.php
+++ favoritos.php
@@ -11,37 +11,22 @@
 		<h1>Estos son tus favoritos!</h1>
 		<?php
 
-		include_once ('BD/GestorBD.php');
+		include_once ('BD/usuario.php');
 
-		// session_start();
-		// if(!$_SESSION['logged'])
-		// header('Location:index.php');
+		//Crear objeto usuario
+		$usuario = new Usuario($_GET["idUsuario"]);
+		$favoritos = $usuario->getFavoritos();
+		//Comprobar si ha habido errores
+		if($usuario->error() == -2) //No pudo conectar
+			header('Location:index.php?err_bd');	//Redirecconar con GET a error
+		else if($usuario->error() == -1)//no existe el usuario (o ha fallado la consulta)
+			header('Location:errores.php?error="userNotFound"');
 
-		$bd = new GestorBD();
-
-		$idusuarioactual = 4;
-		//POner un Id que exista
-
-		if ($bd -> conectar()) {
-
-			$query = sprintf("SELECT * FROM favoritos WHERE idUsuario1= '%s'", $idusuarioactual);
-			$tuplas = $bd -> consulta($query);
-			while ($fila = mysql_fetch_assoc($tuplas)) {
-				$idUsuario2 = $fila['idUsuario2'];
-
-				$query = sprintf("SELECT alias FROM usuarios WHERE idUsuario= '%s'", $idUsuario2);
-				$aux = $bd -> consulta($query);
-				
-				
-				while ($fila = mysql_fetch_assoc($aux)) {
-					$alias = $fila['alias'];
-					$span= sprintf("<span><a href='infoUsuario.php?idUsuario=%s'>%s</a></span>\n\t\t", $idUsuario2,$alias);
-					echo $span;
-				}
-			}
-
+		foreach($favoritos as $fav){
+			$span= sprintf("<span><a href='infoUsuario.php?idUsuario=%s'>%s</a></span>\n\t\t", $fav['idUsuario'],$fav['alias']);
+			echo $span;
 		}
-	?>
+?>
 	</div>
 	</body>
 </html>
