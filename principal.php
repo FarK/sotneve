67695eef35cfd2731181fa8d0d7041838587f5ef
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-14 20:41:11 +0000

    en principal.php  he puesto ya los favoritos. getFavoritos() y foreach copiado de favoritos.php, nada importante

--- principal.php
+++ principal.php
@@ -1,13 +1,12 @@
 <?php
-	include ("includes/testSession.php");
-	include_once('BD/GestorBD.php');
-	include_once('BD/usuario.php');
+include ("includes/testSession.php");
+include_once ('BD/GestorBD.php');
+include_once ('BD/usuario.php');
 
-	//Creamos un objeto usuario con el usuario logeado
-	$usuarioActual = new Usuario($_SESSION['idUsuario']);
-	if($usuarioActual->error() != 0)
-		header('Location:errores.php?error="usernotfound"');
-	
+//Creamos un objeto usuario con el usuario logeado
+$usuarioActual = new Usuario($_SESSION['idUsuario']);
+if ($usuarioActual -> error() != 0)
+	header('Location:errores.php?error="usernotfound"');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -18,33 +17,41 @@
 	</head>
 	<body>
 		<!-- Incluimos la cabecera -->
-		<?php include ("includes/head.php"); 
-		include ("buscarevento.php")?>
+		<?php include ("includes/head.php");
+include ("buscarevento.php")
+		?>
 
-		<div id="contenedor"> 
+		<div id="contenedor">
 			<div id="cabecera">
 				<h1>Cabecera</h1>
-			</div> 
-			<div id="wrapper"> 
-				<div id="eventos"> 
-					<p><strong>Eventos en [Sevilla(TODO)]</strong></p>
-				</div> 
-			</div> 
-			
-			<div id="favoritos"> 
-				<p><strong>Tus favoritos</strong></p>
+			</div>
+			<div id="wrapper">
+				<div id="eventos">
+					<p>
+						<strong>Eventos en [Sevilla(TODO)]</strong>
+					</p>
+				</div>
+			</div>
+			<div id="favoritos">
+				<p>
+					<strong>Tus favoritos</strong>
+				</p>
 				<?php
-				
+				$favoritos = $usuarioActual -> getFavoritos();
+				foreach ($favoritos as $fav) {
+					$span = sprintf("<span><a href='infoUsuario.php?idUsuario=%s'>%s</a></span>\n\t\t", $fav['idUsuario'], $fav['alias']);
+					echo $span;
+				}
 				?>
-			</div> 
-			
-			<div id="eventosUsuario"> 
-				<p><strong>Tus eventos</strong></p>
-			</div> 
-			
-		<div id="pie">
-			<span>Pie</span>
-		</div> 
-	</div> 
+			</div>
+			<div id="eventosUsuario">
+				<p>
+					<strong>Tus eventos</strong>
+				</p>
+			</div>
+			<div id="pie">
+				<span>Pie</span>
+			</div>
+		</div>
 	</body>
 </html>
