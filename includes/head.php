74b03dbd03a17e681690481e2c05caa2a8b88661
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-15 18:35:48 +0000

    Poniendo estilo a head.php

--- includes/head.php
+++ includes/head.php
@@ -8,14 +8,19 @@
 		header('Location:index.php?err_bd');	//Redirecconar con GET a error
 ?>
 
-<div id="cabecera">
-	<div id="logo">
+<div id="hcabecera">
+	<div id="hlogo">
 		<h1>logo</h1>
 	</div>
-	<div id="usuario">
+	<div id="husuario">
 	<span>hola, <?php echo $usuario->getCampo("nombre")." ".$usuario->getCampo("apellidos"); ?>;</span>
-	<a id="linkperfil" href="evento.css">Mi perfil</a>
+	<a id="hlinkperfil" href="evento.css">Mi perfil</a>
 	<a href="formulario.xhtml">Logout</a>
+	</div>
+	
+	<div id="hbotones">
+		<a id="hinicio" href="principal.php">Inicio</a>
+		<a id="hfavoritos" href="favoritos.php">Favoritos</a>
 	</div>
 
 	<?php include ("buscarevento.php"); ?>
