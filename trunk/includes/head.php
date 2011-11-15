0e5c63a41af219f157038c695ad2701c477bb467
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-15 20:23:03 +0000

    retocando estilo de head.php

--- trunk/includes/head.php
+++ trunk/includes/head.php
@@ -1,11 +1,12 @@
 <?php
-	include_once("BD/usuario.php");
+include_once ("BD/usuario.php");
 
-	//Creamos el objeto usuario del usuario conectado
-	$usuario = new Usuario($_SESSION['idUsuario']);
-	//Comprobar si ha habido errores
-	if($usuario->error() == -2) //No pudo conectar
-		header('Location:index.php?err_bd');	//Redirecconar con GET a error
+//Creamos el objeto usuario del usuario conectado
+$usuario = new Usuario($_SESSION['idUsuario']);
+//Comprobar si ha habido errores
+if ($usuario -> error() == -2)//No pudo conectar
+	header('Location:index.php?err_bd');
+//Redirecconar con GET a error
 ?>
 
 <div id="hcabecera">
@@ -13,15 +14,17 @@
 		<h1>logo</h1>
 	</div>
 	<div id="husuario">
-	<span>hola, <?php echo $usuario->getCampo("nombre")." ".$usuario->getCampo("apellidos"); ?>;</span>
-	<a id="hlinkperfil" href="evento.css">Mi perfil</a>
-	<a href="formulario.xhtml">Logout</a>
+		<span>hola, <?php echo $usuario -> getCampo("nombre") . " " . $usuario -> getCampo("apellidos");?>;</span>
+		<a id="hlinkperfil" href="evento.css">Mi perfil</a>
+		<a href="formulario.xhtml">Logout</a>
 	</div>
-	
 	<div id="hbotones">
 		<a id="hinicio" href="principal.php">Inicio</a>
 		<a id="hfavoritos" href="favoritos.php">Favoritos</a>
 	</div>
-
-	<?php include ("buscarevento.php"); ?>
+	<div id="hlugarbuscador">
+		<?php
+		include ("buscarevento.php");
+		?>
+	</div>
 </div>
