af3ce7d8554fb0ba7fd3508e05fdb432783b2aca
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-16 16:49:19 +0000

    head.css y buscarevento.css ahora estan unificados en general.css

--- principal.php
+++ principal.php
@@ -14,8 +14,7 @@ if ($usuarioActual -> error() != 0)
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" type="text/css" href="styles/principal.css" />
-		<link rel="stylesheet" type="text/css" href="styles/buscarevento.css">
-		<link rel="stylesheet" type="text/css" href="styles/head.css">
+		<link rel="stylesheet" type="text/css" href="styles/general.css">
 		<script type="text/javascript" src="scripts/buscarevento.js"></script>
 	</head>
 	<body>
