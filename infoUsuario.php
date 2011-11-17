1844635e29d29dc88ce4e2ad910e41d2f04b1bb1
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-17 11:35:13 +0000

    Ahora si funciona y esta completo infoUsuario.php

--- infoUsuario.php
+++ infoUsuario.php
@@ -2,13 +2,13 @@
 	include ("includes/testSession.php");
 	include_once('BD/usuario.php');
 
-	//Crear objeto usuario
-	$usuario = new Usuario($_GET["idUsuario"]);
+	//Se llama usuario visitado porque si no se pisa con usuario de head.php
+	$usuarioVisitado = new Usuario($_GET["idUsuario"]);
 	//Comprobar si ha habido errores
-	if($usuario->error() == -2) //No pudo conectar
+	if($usuarioVisitado->error() == -2) //No pudo conectar
 		header('Location:index.php?err_bd');	//Redirecconar con GET a error
 	
-	else if($usuario->error() == -1)//no existe el usuario (o ha fallado la consulta)
+	else if($usuarioVisitado->error() == -1)//no existe el usuario (o ha fallado la consulta)
 		header('Location:errores.php?error="userNotFound"');
 
 
@@ -20,46 +20,115 @@
 	<head>
 		<!-- IMPORTANTE ESA LÍNEA DE ABAJO!!!  -->
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>Sotneve - <?php echo $usuario->getCampo("alias"); ?></title>
+		<title>Sotneve - <?php echo $usuarioVisitado->getCampo("alias"); ?></title>
 		<link rel="stylesheet" type="text/css" href="styles/index.css" />
 	</head>
 	<body>
 		<!-- Incluimos la cabecera -->
 		<?php include ("includes/head.php"); ?>
 
-		<h1><?php echo $usuario->getCampo("alias")?></h1>
+		<h1><?php echo $usuarioVisitado->getCampo("alias")?></h1>
 		
 		<div class="info_usuario">
-			<span id="nombre" class="campo_usuario">
+			
 				<?php
-					if($usuario->esVisible($NOMBRE)) echo $usuario->getCampo("nombre");
+				
+					$novisible="No Disponible";
+					$nombre=$novisible;
+					$apellidos=$novisible;
+					$sexo=$novisible;
+					$fechaNac=$novisible;
+					$email=$novisible;
+					
+					
+					if($usuarioVisitado->esVisible($NOMBRE)){
+						$nombre=$usuarioVisitado->getCampo("nombre");
+						
+					}
+
+					if($usuarioVisitado->esVisible($APELLIDOS)){
+						$apellidos=$usuarioVisitado->getCampo("apellidos");
+						
+					}
+
+					if($usuarioVisitado->esVisible($SEXO)) {
+						$sexo=$usuarioVisitado->getCampo("sexo");
+						
+						if($sexo==1){
+							$sexo="Hombre";
+						}elseif($sexo==0){
+							$sexo="Mujer";
+						}
+					}
+
+					if($usuarioVisitado->esVisible($FECHA_NAC)){
+						 $fechaNac=$usuarioVisitado->getCampo("fechaNac");
+					}
+
+					if($usuarioVisitado->esVisible($EMAIL)) {
+						$email=$usuarioVisitado->getCampo("email");
+					}
+
+					$provincia=$usuarioVisitado->getCampo("provincia");
+					$alias=$usuarioVisitado->getCampo("alias");
+					
+					
+					
+				?>
+			<span class="l_usuario"> 
+				Nombre:
+			</span>
+			<span class="c_usuario">
+				<?php
+				echo $nombre;
 				?>
 			</span>
-			<span id="apellidos" class="campo_usuario">
+			
+			<span class="l_usuario"> 
+				Apellidos:
+			</span>
+			<span class="c_usuario">
 				<?php
-					if($usuario->esVisible($APELLIDOS)) echo $usuario->getCampo("apellidos");
+				echo $apellidos;
 				?>
 			</span>
-			<span id="sexo" class="campo_usuario">
+			
+			<span class="l_usuario"> 
+				Sexo:
+			</span>
+			<span class="c_usuario">
 				<?php
-					if($usuario->esVisible($SEXO)) echo $usuario->getCampo("sexo");
+				echo $sexo;
 				?>
 			</span>
-			<span id="fechaNac" class="campo_usuario">
+			
+			<span class="l_usuario"> 
+				Fecha de Nacimiento:
+			</span>
+			<span class="c_usuario">
 				<?php
-					if($usuario->esVisible($FECHA_NAC)) echo $usuario->getCampo("fechaNac");
+				echo $fechaNac;
 				?>
 			</span>
-			<span id="email" class="campo_usuario">
+			
+			<span class="l_usuario"> 
+				Email:
+			</span>
+			<span class="c_usuario">
 				<?php
-					if($usuario->esVisible($EMAIL)) echo $usuario->getCampo("email");
+				echo $email;
 				?>
 			</span>
-			<span id="provincia" class="campo_usuario">
+			
+			<span class="l_usuario"> 
+				Provincia:
+			</span>
+			<span class="c_usuario">
 				<?php
-					echo $usuario->getCampo("provincia");
+				echo $provincia;
 				?>
 			</span>
+			
 		</div>
 	</body>
 </html>
