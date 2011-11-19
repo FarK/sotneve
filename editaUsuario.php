27658315fb3cffe06f1af607b7fd5633c034af9a
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-11-19 18:49:54 +0000

    pequeños detalles para preparar el java script

--- editaUsuario.php
+++ editaUsuario.php
@@ -6,50 +6,49 @@ include_once ('BD/usuario.php');
 $bd = new GestorBD();
 
 //Creamos un objeto usuario con el usuario logeado
-$usuario= new Usuario($_SESSION['idUsuario']);
-if ($usuario -> error() != 0){
-		header('Location:errores.php?error="usernotfound"');
+$usuario = new Usuario($_SESSION['idUsuario']);
+if ($usuario -> error() != 0) {
+	header('Location:errores.php?error="usernotfound"');
 }
-function fechaNac($usuario){
-		
-	
-	$fechanac=$usuario->getCampo('fechaNac');	
+function fechaNac($usuario) {
+
+	$fechanac = $usuario -> getCampo('fechaNac');
 	$ano = substr($fechanac, 0, 4);
-	$mes = substr($fechanac, 5,2);
+	$mes = substr($fechanac, 5, 2);
 	$dia = substr($fechanac, 8, 9);
-	
-	echo $dia."/".$mes."/".$ano;
+
+	echo $dia . "/" . $mes . "/" . $ano;
 }
 
-function provinciaActual($bd,$usuario){
-	$valor=$usuario->getCampo('provincia');
-	$query=sprintf("SELECT nombre FROM provincias WHERE idProvincia=%s",$valor);
-	$tuplas=$bd->consulta($query);
-		
-	while ($fila = mysql_fetch_assoc($tuplas)) { 
-			$valor=$fila['nombre'];
+function provinciaActual($bd, $usuario) {
+	$valor = $usuario -> getCampo('provincia');
+	$query = sprintf("SELECT nombre FROM provincias WHERE idProvincia=%s", $valor);
+	$tuplas = $bd -> consulta($query);
 
-		}
+	while ($fila = mysql_fetch_assoc($tuplas)) {
+		$valor = $fila['nombre'];
 
-	$linea=sprintf(" Actualmente %s",$valor);
-	echo $linea;
-}
-
-function creaPlaceHolder($usuario,$campo){
-	
-	$valor=$usuario->getCampo($campo);
-	$linea=sprintf("placeholder='%s'",$valor);
-	echo $linea;
-}
-
-function creaCheckBox($usuario,$campo){
-	$visibilidad=$usuario->esVisible($campo);
-	$visible="";
-	$check="check";
-	if($visibilidad){ 
-		$visible="checked=''";
 	}
-	$linea=sprintf("<input type='checkbox' id='%s%s' value='%s%s' %s>" ,$check,$campo,$check,$campo,$visible);
+
+	$linea = sprintf(" Actualmente %s", $valor);
+	echo $linea;
+}
+
+function creaPlaceHolder($usuario, $campo) {
+
+	$valor = $usuario -> getCampo($campo);
+	$linea = sprintf("placeholder='%s'", $valor);
+	echo $linea;
+}
+
+function creaCheckBox($usuario, $campo) {
+	$visibilidad = $usuario -> esVisible($campo);
+	$visible = "";
+	$check = "check";
+	if ($visibilidad) {
+		$visible = "checked=''";
+	}
+	$linea = sprintf("<input type='checkbox' id='%s%s' value='%s%s' %s>", $check, $campo, $check, $campo, $visible);
 	echo $linea;
 }
 ?>
@@ -61,37 +60,49 @@ function creaCheckBox($usuario,$campo){
 		<title>sotneve - Únete</title>
 		<script type="text/javascript" src="scripts/editausuario.js"></script>
 		<link rel="stylesheet" type="text/css" href="styles/general.css">
-		<?php include ("includes/head.php"); ?>
+		<?php
+			include ("includes/head.php");
+		?>
 	</head>
 	<body>
 		<form name="form" method="post" action="insertarcambiosperfil.php" onsubmit="return esFormularioValido()">
 			<fieldset>
+				<?php
+				if (isset($_GET['err_email'])) {
+					echo '<span id="erroremail">El email ya existe o es superior a 60 caracteres</span>';
+				}
+				if (isset($_GET['err_contrasena'])) {
+					echo '<span id="errorcontrasena">Contraseña incorrecta, ambas contraseña deben de coincidir y ser superior a 6 caracteres e inferior a 15</span>';
+				}
+				if (isset($_GET['err_campos'])) {
+					echo '<span id="errorcampos">Todos los campos son obligatorios</span>';
+				}
+				?>
 				<h1>Edita tu perfil</h1>
+				<span id="errores">Errores.</span>
 				<hr />
 				<div class="div1">
 					<label for="sexo" class="labelright">Sexo:</label>
 					<select  name="sexo" id="sexo" class="inputright">
-						<?php $sexo=$usuario->getCampo('sexo');
-						if($sexo==1){
+						<?php $sexo = $usuario -> getCampo('sexo');
+						if ($sexo == 1) {
 							echo "<option value='1'>Hombre</option>
-						          <option value='0'>Mujer</option>";
-						}else{
+<option value='0'>Mujer</option>";
+						} else {
 							echo "<option value='0'>Mujer</option>
-						          <option value='1'>Hombre</option>";
+<option value='1'>Hombre</option>";
 						}
-						 ?>
-
+						?>
 					</select>
-					<?php creaCheckBox($usuario,$SEXO);?>
+					<?php creaCheckBox($usuario, $SEXO);?>
 				</div>
 				<div class="div2">
 					<label id="idnombre" for="nombre" class="labelleft" >Nombre:</label>
 					<input type="text" name="nombre" id="nombre" class="inputleft" onblur="esCampoNoVacio(this.id)" <?php creaPlaceHolder($usuario, 'nombre')?>/>
-					
-					<?php creaCheckBox($usuario,$NOMBRE);?>
+					<?php creaCheckBox($usuario, $NOMBRE);?>
 					<label class="labelright" for="apellidos">Apellidos:</label>
 					<input type="text" name="apellidos" id="apellidos" class="inputright" onblur="esCampoNoVacio(this.id)" <?php creaPlaceHolder($usuario, 'apellidos')?> />
-					<?php creaCheckBox($usuario,$APELLIDOS);?>
+					<?php creaCheckBox($usuario, $APELLIDOS);?>
 				</div>
 				<div class="div3">
 					<label class="labelleft" for="contrasenaactual">Contraseña Actual:</label>
@@ -104,40 +115,39 @@ function creaCheckBox($usuario,$campo){
 				<div class="div4">
 					<label class="labelleft" for="email">Email:</label>
 					<input type="text" name="email" id="email" onblur="esEmailValido()"  <?php creaPlaceHolder($usuario, 'email')?>/>
-					<?php creaCheckBox($usuario,$EMAIL);?>
+					<?php creaCheckBox($usuario, $EMAIL);?>
 					<label class="labelright">Fecha de nacimiento:</label>
 					<input type="text" name="fechanac" id="fechanac"  placeholder="<?php fechaNac($usuario)?>"/>
-					<?php creaCheckBox($usuario,$FECHA_NAC);?>
+					<?php creaCheckBox($usuario, $FECHA_NAC);?>
 				</div>
 				<div class="div5">
 					<label class="labelleft" for="provincia">Provincia:</label>
 					<select  name="provincia" id="provincia">
-						<option value="0"><?php 
+						<option value="0"><?php
 						if ($bd -> conectar()) {
-							provinciaActual($bd,$usuario);
-							$bd->desconectar();} ?></option>
+							provinciaActual($bd, $usuario);
+							$bd -> desconectar();
+						}
+							?></option>
 						<?php
-						
 
 						//Conectar a la bd
 						if ($bd -> conectar()) {
-						$query=sprintf("SELECT idProvincia, nombre FROM provincias");
-						$tuplas=$bd->consulta($query);
-						while ($fila = mysql_fetch_assoc($tuplas)) {
-							$idProvincia = $fila['idProvincia'];
-							$nombre=$fila['nombre'];
-							$option=sprintf('<option value="%s">%s</option>',$idProvincia,$nombre);
-							echo $option;
-						}
-						$bd->desconectar();
-						}else{
+							$query = sprintf("SELECT idProvincia, nombre FROM provincias");
+							$tuplas = $bd -> consulta($query);
+							while ($fila = mysql_fetch_assoc($tuplas)) {
+								$idProvincia = $fila['idProvincia'];
+								$nombre = $fila['nombre'];
+								$option = sprintf('<option value="%s">%s</option>', $idProvincia, $nombre);
+								echo $option;
+							}
+							$bd -> desconectar();
+						} else {
 							//Error aqui cuando aclaremos que vamos hacer con ellos
 						}
-							
 						?>
 					</select>
 				</div>
-
 				<button type="submit" id="registrate">
 					Guardar
 				</button>
