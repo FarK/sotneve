01a22deccebd80bc5ffbc406e4b3e1336c6b6e08
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-12-27 17:14:43 +0000

    Añadido logo en index.php

--- trunk/index.php
+++ trunk/index.php
@@ -1,5 +1,5 @@
 <?php
-	session_start();
+session_start();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <!-- IMPORTANTE ESA LÍNEA DE AHÍ ARRIBA Y LA DE ABAJO!!!  -->
@@ -12,18 +12,23 @@
 		<script type="text/javascript" src="scripts/index.js"></script>
 	</head>
 	<body>
-		<form id="login_form" method="post" action="login.php" onsubmit="return validaForm()"> 
+		<div id="logoprincipal">
+			<IMG ID="logop" SRC="images/logo.jpg" ALT="Inicio">
+		</div>
+		<form id="login_form" method="post" action="login.php" onsubmit="return validaForm()">
 			<fieldset>
-				<legend>Login</legend> 
+				<legend>
+					Login
+				</legend>
 				<h1>BIENVENIDO A SOTNEVE</h1>
 				<span class="error">Inserte un correo electr&oacute;nico v&aacute;lido</span>
 				<span class="error">El campo contrase&ntilde;a no puede estar vac&iacute;o</span>
 				<?php
-				if(isset($_SESSION['err_pass']) && $_SESSION['err_pass']){
+				if (isset($_SESSION['err_pass']) && $_SESSION['err_pass']) {
 					echo "<span class='errorserv' >Usuario o contrase&ntilde;a incorrecto</span>";
 					$_SESSION['err_pass'] = false;
 				}
-				if(isset($_GET['err_bd']) && $_SESSION['err_bd']){
+				if (isset($_GET['err_bd']) && $_SESSION['err_bd']) {
 					echo "<span class='errorserv'>No se pudo conectar a la base de datos.</span>";
 					$_SESSION['err_bd'] = false;
 				}
@@ -36,9 +41,7 @@
 				<input id="pass" name="pass" type="password" placeholder="Contrase&ntilde;a" onblur="validaPass()"/>
 				<input id="send" class="boton" name="Send" type="submit" value="Entrar"/>
 				<a id="registrarse" href="registro.php">Registrarse</a>
-				
 			</fieldset>
 		</form>
-
 	</body>
 </html>
