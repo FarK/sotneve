031d447de7ea6f3847ba9068821cc0c1d46aef96
fark.zano@gmail.com <fark.zano@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-12-27 19:13:23 +0000

    Creando la nueva capa de datos

---
+++ branches/GestorBD/BD/conexion.php
@@ -0,0 +1,116 @@
+<?php
+class Conexion{
+	private $PDO;
+	private $consultas;		//Array con consultas preparadas
+	private $DB_HOST;
+	private $DB_USERNAME;
+	private $DB_PASS;
+	private $DB_NAME;
+	private $conn;
+
+	public function __construct(){
+		//Inicializamos los atributos
+		$this->PDO = NULL;
+		$this->consultas = array();
+		$this->DB_HOST = 'localhost';
+		$this->DB_USERNAME = 'root';
+		$this->DB_PASS = '';
+		$this->DB_NAME = 'sotneve';
+		$this->conn = NULL;
+
+		try{
+			$this->PDO = new PDO("mysql:host=$this->DB_HOST;dbname=$this->DB_NAME",
+				$this->DB_USERNAME, $this->DB_PASS);
+			$this->PDO->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+		}
+		catch(PDOException $exp){
+			//TODO: Redirigir a página de error
+			echo "ERROR AL CONECTARSE CON LA BD </br>";
+			echo $exp;
+		}
+	}
+
+	//TODO: Intentar hacer esto en el destructor
+	public function desconectar(){
+		$this->PDO = null;
+	}
+
+	public function prepare($consulta){
+		return $this->PDO->prepare($consulta);
+	}
+
+	public function bindParam($stmt, $param, $value){
+		$stmt->bindParam($param, $value);
+	}
+
+	//Ejecuta todas las consultas preparadas
+	public function ejecutarPreparada($consultaPreparada){
+		try{
+			$consultaPreparada->execute();
+		}
+		catch(PDOException $exp){
+			//TODO: Redirigir a página de error
+			echo "ERROR AL HACER LA CONSULTA PREPARADA</br>";
+			echo $exp;
+		}
+	}
+
+	/**************************************************************************************************************************************************************************************/
+	/* TODO: Mover estas consultas a sus clases correspondientes                                                                                                                          */
+	/**************************************************************************************************************************************************************************************/
+	public function passCorrecta($email, $pass) {
+		$query = sprintf("SELECT * FROM usuarios WHERE email = '%s' AND pass = SHA2('%s', 256)", $email, $pass);
+		$result = $this -> consulta($query);
+		if (mysql_num_rows($result) == 1) {
+			$row = mysql_fetch_assoc($result);
+			return $row;
+		} else {
+			return false;
+		}
+	}
+
+	public function insertarUsuario($fechanac, $sexo, $email, $alias, $contrasena, $nombre, $apellidos, $provincia) {
+		$query = sprintf("INSERT INTO usuarios (fechaNac, sexo, email, alias, pass, nombre, apellidos, provincia,visibilidad) 
+			VALUES ('%s', '%s', '%s', '%s', SHA2('%s',256), '%s', '%s', '%s', '%s' )", $fechanac, $sexo, $email, $alias, $contrasena, $nombre, $apellidos, $provincia, 0);
+		$res = $this -> consulta($query);
+		var_dump($query);
+
+	}
+
+
+	public function insertarEvento($fechaEvento, $titulo, $numpersonas, $provincia, $descripcion, $lugar) {
+		$query = sprintf("INSERT INTO eventos (fechaEvento, titulo, numpersonas, provincia, descripcion,lugar) 
+			VALUES ('%s', '%s', '%s', '%s', '%s', '%s' )", $fechaEvento, $titulo, $numpersonas, $provincia, $descripcion, $lugar);
+		$res = $this -> consulta($query);
+		var_dump($query);
+	}
+
+	public function usuariosCon($campo, $elemento) {
+		$query = sprintf("SELECT * FROM usuarios WHERE '%s' = '%s'", $campo, $elemento);
+		return $this -> consulta($query);
+	}
+
+	public function getNombreElementoCon($tabla,$elemento,$id) {
+		$aux = sprintf("SELECT nombre FROM %s WHERE %s=%s", $tabla,$elemento, $id);
+		$provinciaAux = $this -> consulta($aux);
+		while ($fila = mysql_fetch_assoc($provinciaAux)) {
+			return $provinciaString = $fila['nombre'];
+		}
+	}
+
+	public  function getEventosConProvinciaYSubtipoNoCaducados($provincia,$subtipo){
+		$fecha = time (); 
+		$actual =  date ( "Y-m-d h:i:s" , $fecha );
+		$query = sprintf("SELECT * FROM eventos WHERE idProvincia='%s' AND idSubTipo='%s' AND fechaEvento>='%s'", $provincia, $subtipo, $actual);
+		$tuplas = $this -> consulta($query);
+		return $tuplas;
+	}
+	public  function getTodosEventosConProvinciaNoCaducados($provincia,$subtipo){
+		$fecha = time (); 
+		$actual =  date ( "Y-m-d h:i:s" , $fecha );
+		$query = sprintf("SELECT * FROM eventos WHERE idProvincia='%s' AND fechaEvento>='%s'", $provincia, $actual);
+		$tuplas = $this -> consulta($query);
+		return $tuplas;
+	}
+}
+?>
