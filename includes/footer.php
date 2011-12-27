fd7849f1b1b9b000e98fedea0f48218efcf41021
rafaespillaque@gmail.com <rafaespillaque@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-12-27 18:07:09 +0000

    Footer cambiado para paginas online

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,14 @@
 <div id="pie">
 	<span>Copyright Sotneve 2011 &copy;</span>
 </div>
+	<!--	
+	SÓLO SIRVE PARA PÁGINAS ONLINE
+	<div class="ja-cert">
+    <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank" title="Our site is valid CSS" style="text-decoration: none;">
+		<img src="http://www.informatica.us.es/templates/ja_purity/images/but-css.gif" border="none" alt="Our site is valid CSS" />
+		</a>
+		<a href="http://validator.w3.org/check/referer" target="_blank" title="Our site is valid XHTML 1.0 Transitional" style="text-decoration: none;">
+		<img src="http://www.informatica.us.es/templates/ja_purity/images/but-xhtml10.gif" border="none" alt="Our site is valid XHTML 1.0 Transitional" />
+		</a>
+	</div>
+	-->
