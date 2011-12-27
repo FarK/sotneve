90aaa34161b3aa90e5d499e7413cff560c5c5cfc
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2011-12-27 19:10:46 +0000

    Añadido acerca.php con su css y sus videos

--- trunk/includes/footer.php
+++ trunk/includes/footer.php
@@ -1,6 +1,8 @@
 <div id="pie">
 	<span>Copyright Sotneve 2011 &copy;</span>
 	
+		<a id="acerca" href="acerca.php">Acerca de nosotros</a>
+	
 </div>
 <div id="tw"><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script></div>
