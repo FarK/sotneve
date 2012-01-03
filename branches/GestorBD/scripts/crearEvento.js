b6b074093e0d3350afbccc6c769dbe860e1b6fab
rafaespillaque@gmail.com <rafaespillaque@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-03 15:02:07 +0000

    Me esta matando, falta por hacer

--- branches/GestorBD/scripts/crearEvento.js
+++ branches/GestorBD/scripts/crearEvento.js
@@ -1,11 +1,26 @@
 function valida() {
-
 	var factual = new Date()
 	var dia = factual.getUTCDate()
 	var ano = factual.getFullYear();
 	var mes = factual.getMonth();
-
-	if(document.getElementById("numpersonas").value <= 1) {
+	
+	var titulo = document.getElementById("titulo").value;
+	var provincia = document.getElementById("provincia").value;
+	var lugar = document.getElementById("lugar").value;
+	var descripcion = document.getElementById("descripcion").value;
+	var numpersonas = document.getElementById("numpersonas").value;
+	
+	if(titulo == "" || lugar == "" || descripcion == "" || numpersonas = ""){
+		alert("Debe rellenar todos los campos");
+		return false;
+	}
+	
+	if(provincia == 0){
+		alert("Debe seleccionar una provincia");
+		return false;
+	}
+	
+	if(numpersonas <= 1) {
 		alert("El numero de personas debe ser mayor que 1");
 		return false;
 	}
