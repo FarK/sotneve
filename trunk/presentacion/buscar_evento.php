677ea97d3bc9b70ccc78b03a19c5e2cb7135fb44
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-04 19:28:51 +0000

    Nuevo buscador implementado, el mio con ajax era mas bonito mamones

--- trunk/presentacion/buscar_evento.php
+++ trunk/presentacion/buscar_evento.php
@@ -1,10 +1,13 @@
 <?php
 include_once ('../datos/conexion.php');
 include_once ('../datos/provincia.php');
+include_once ('../datos/tipo.php');
 
 $conexion = new Conexion();
 $provincia = new Provincia($conexion);
-$prov=$provincia->getProvincias();
+$prov = $provincia -> getProvincias();
+
+$tipo = new Tipo($conexion);
 
 // function generaOption($bd, $campo, $tabla) {//el metodo ya no es generico
 //
@@ -21,46 +24,37 @@ $prov=$provincia->getProvincias();
 // }
 
 // function generaTipos() {
-// 
-	// $consulta = mysql_query("SELECT idTipo, nombre FROM tipos");
-// 
-	// // Voy imprimiendo el primer select compuesto por los tipos
-	// echo "<select class='selbusc' name='tipos' id='tipos' onchange='cargaContenido(this.id)'>";
-	// echo "<option value='0'>Elige</option>";
-	// while ($registro = mysql_fetch_row($consulta)) {
-		// echo "<option value='" . $registro[0] . "'>" . $registro[1] . "</option>";
-	// }
-	// echo "</select>";
+//
+// $consulta = mysql_query("SELECT idTipo, nombre FROM tipos");
+//
+// // Voy imprimiendo el primer select compuesto por los tipos
+// echo "<select class='selbusc' name='tipos' id='tipos' onchange='cargaContenido(this.id)'>";
+// echo "<option value='0'>Elige</option>";
+// while ($registro = mysql_fetch_row($consulta)) {
+// echo "<option value='" . $registro[0] . "'>" . $registro[1] . "</option>";
+// }
+// echo "</select>";
 // }
 ?>
 
-<form method="get" action="generabusqueda.php">
+<form method="get" action="../logica/busqueda.php">
 	<div id="buscador" style="width:600px;">
-<!-- 		                <div class='opcion'>
-		<select  name="provincia" id="provincia"> -->
-		<?php
-		// if ($conectado) {
-		// generaOption($bd, 'nombre', 'provincias');
-		// }
-		?>
-<!-- 		</select>
-		</div>  -->
 		<div class='opcion'>
-			<select class='selbusc' name='tipos' id='tipos' onchange='cargaContenido(this.id)'>
+			<select class='selbusc' name='provincia' id='provincia'>
 				<option value='0'>Elige</option>
 				<?php
-				
-				foreach ($prov as $idp=>$p) {
-					
+
+				foreach ($prov as $idp => $p) {
+
 					echo "<option value='" . $idp . "'>" . $p . "</option>";
 				}
-				
 				?>
 			</select>
 		</div>
 		<div class='opcion'>
-			<select disabled="disabled" name="subtipos" id="subtipos">
-				<option value="0">Selecciona opci&oacute;n...</option>
+			<select name="tipo" id="tipo">
+				<option value="0">Elige</option>
+				<?php $tipo->getArbolTipos();?>
 			</select>
 		</div>
 		<input type="image" id="search_icon" src="recursos/imagenes/search.png" alt="Buscar eventos"/>
