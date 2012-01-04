661cfaf78ca93cecc148f7b77a75ab44be60fc71
fark.zano@gmail.com <fark.zano@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-04 11:57:24 +0000

    Refactorizando carpetas: insertando test_session

--- presentacion/acerca.php
+++ presentacion/acerca.php
@@ -2,15 +2,15 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
 	<head>
 		<meta charset=utf-8 />
-		<link rel="stylesheet" type="text/css" href="estilos/acerca.css" />
-		<script type="text/javascript" src="../logica/scripts/buscarevento.js"></script>
+		<link rel="stylesheet" type="text/css" href="styles/acerca.css" />
+		<script type="text/javascript" src="scripts/buscarevento.js"></script>
 	</head>
 	<body>
 		<div id="cabecera">
 			<?php
 			session_start();
 			if (isset($_SESSION['idUsuario'])) {
-				include ("head.php");
+				include ("includes/head.php");
 			}else{
 				//TODO desarrollar un boton de volver, una barra arriba con opciones inicio, registrarse o algo parecido, que esta muy soso
 			}
@@ -20,11 +20,11 @@
 		<div>
 			<span id="nosotros"> Esta p&aacute;gina ha sido desarrollada por un grupo de 5 compa&ntilde;eros para la asignatura de ABD. </span>
 			<video id="video" controls>
-				<source src="videos/tu_video.ogv" type="video/ogg" />
-				<source src="videos/tu_video.mp4" type="video/mp4" />
+				<source src="recursos/videos/tu_video.ogv" type="video/ogg" />
+				<source src="recursos/videos/tu_video.mp4" type="video/mp4" />
 				<object width="160" height="90" data="video.flv">
 					<param name="movie" value="tu_video.flv">
-					<embed src="videos/tu_video.flv" width="160" height="90">
+					<embed src="recursos/videos/tu_video.flv" width="160" height="90">
 				</object>
 				<p>
 					Tu navegador no soporta HTML5 ni Flash Â¿En que era estÃ¡s?
