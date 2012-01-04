1585249b43e53367e878cba90622e4260da3bbcc
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-04 19:29:30 +0000

    Quitado comentarios de buscar_evento.php

--- presentacion/buscar_evento.php
+++ presentacion/buscar_evento.php
@@ -1,4 +1,5 @@
 <?php
+
 include_once ('../datos/conexion.php');
 include_once ('../datos/provincia.php');
 include_once ('../datos/tipo.php');
@@ -8,33 +9,6 @@ $provincia = new Provincia($conexion);
 $prov = $provincia -> getProvincias();
 
 $tipo = new Tipo($conexion);
-
-// function generaOption($bd, $campo, $tabla) {//el metodo ya no es generico
-//
-// $query = sprintf("SELECT %s,idProvincia FROM %s ORDER BY `nombre`", $campo, $tabla);
-// //  $campos = $bd -> consulta($query);
-//
-// while ($fila = mysql_fetch_assoc($campos)) {
-// $campoAux = $fila[$campo];
-// $campoAux2 = $fila['idProvincia'];
-// $option = sprintf("<option value='%s'>%s</option>\n\t\t", $campoAux2, $campoAux);
-// echo $option;
-// }
-//
-// }
-
-// function generaTipos() {
-//
-// $consulta = mysql_query("SELECT idTipo, nombre FROM tipos");
-//
-// // Voy imprimiendo el primer select compuesto por los tipos
-// echo "<select class='selbusc' name='tipos' id='tipos' onchange='cargaContenido(this.id)'>";
-// echo "<option value='0'>Elige</option>";
-// while ($registro = mysql_fetch_row($consulta)) {
-// echo "<option value='" . $registro[0] . "'>" . $registro[1] . "</option>";
-// }
-// echo "</select>";
-// }
 ?>
 
 <form method="get" action="../logica/busqueda.php">
@@ -54,7 +28,9 @@ $tipo = new Tipo($conexion);
 		<div class='opcion'>
 			<select name="tipo" id="tipo">
 				<option value="0">Elige</option>
-				<?php $tipo->getArbolTipos();?>
+				
+				<?php $tipo -> getArbolTipos();?>
+				
 			</select>
 		</div>
 		<input type="image" id="search_icon" src="recursos/imagenes/search.png" alt="Buscar eventos"/>
