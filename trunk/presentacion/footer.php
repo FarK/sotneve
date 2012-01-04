72c2a7687e513ef90659d14ab66b428cc14ef1a1
rafaespillaque@gmail.com <rafaespillaque@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-04 12:22:10 +0000

    Quitado twitter de footer

--- trunk/presentacion/footer.php
+++ trunk/presentacion/footer.php
@@ -1,22 +1,4 @@
 <div id="pie">
 	<span>Copyright Sotneve 2011 &copy;</span>
-	
 		<a id="acerca" href="acerca.php">Acerca de nosotros</a>
-	
 </div>
-<div id="tw"><a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
-<script type="javascript">function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
-</script>
-</div>
-
-	<!--	
-	SÓLO SIRVE PARA PÁGINAS ONLINE
-	<div class="ja-cert">
-    <a href="http://jigsaw.w3.org/css-validator/check/referer" target="_blank" title="Our site is valid CSS" style="text-decoration: none;">
-		<img src="http://www.informatica.us.es/templates/ja_purity/images/but-css.gif" border="none" alt="Our site is valid CSS" />
-		</a>
-		<a href="http://validator.w3.org/check/referer" target="_blank" title="Our site is valid XHTML 1.0 Transitional" style="text-decoration: none;">
-		<img src="http://www.informatica.us.es/templates/ja_purity/images/but-xhtml10.gif" border="none" alt="Our site is valid XHTML 1.0 Transitional" />
-		</a>
-	</div>
-	-->
