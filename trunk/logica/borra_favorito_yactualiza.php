b754054e33bc23990173c4d3c51ca2b5ee078891
rafaespillaque@gmail.com <rafaespillaque@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-05 17:19:14 +0000

    TODOsvn add logica/borra_favorito_yactualiza.php  logica/scripts/favoritos.js ! Borrar favoritos funcionando con ajax pero el CSS no he sido capaz

---
+++ trunk/logica/borra_favorito_yactualiza.php
@@ -0,0 +1,30 @@
+<?php
+include 'test_session.php';
+include_once '../datos/conexion.php';
+include_once '../datos/usuario.php';
+
+$idUser = $_SESSION['idUsuario'];
+$idFav = $_GET['idFav'];
+$conex = new Conexion();
+$user = new Usuario($conex, $idUser);
+
+$user->borraFavorito($idFav);
+$favoritos = $user->getFavoritos();
+
+$enlaces = "";
+
+foreach($favoritos as $fav){
+	$echo = sprintf("
+			<div class='favorito'>
+				<a class='favorito' href='info_usuario.php?idUsuario=%s'>%s</a>
+				<form class='favorito' action='javascript:borraFavorito(%s)'>
+					<input id='delete' type='image' src='recursos/imagenes/delete.png'></input>
+				</form>
+			</div>", $fav['idUsuario2'], $fav['alias'], $fav['idUsuario2']);
+	$enlaces = $enlaces . $echo;
+}
+
+echo $enlaces;
+
+$conex->desconectar();
+?>
