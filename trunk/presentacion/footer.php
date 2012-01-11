c660c21e93aa626947a768fc6b288fe8ff9a8a23
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-11 11:16:00 +0000

    Un par de tonterias

--- trunk/presentacion/footer.php
+++ trunk/presentacion/footer.php
@@ -1,4 +1,4 @@
 <div id="pie">
 	<span>Copyright Sotneve 2011 &copy;</span>
-		<a id="acerca" href="acerca.php">Acerca de nosotros</a>
+		<a id="acerca" href="presentacion/acerca.php">Acerca de nosotros</a>
 </div>
