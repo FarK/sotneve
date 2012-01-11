2f0456b2692a209d1bdc1e2f8d46d6a313fac53d
fark.zano@gmail.com <fark.zano@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-11 21:52:51 +0000

    Arreglado enlace a 'Acerca de nosotros' del pie

--- trunk/presentacion/footer.php
+++ trunk/presentacion/footer.php
@@ -1,4 +1,5 @@
 <div id="pie">
-	<span>Copyright Sotneve 2011 &copy;</span>
-		<a id="acerca" href="presentacion/acerca.php">Acerca de nosotros</a>
+	<span class="pieCell">Copyright Sotneve 2011 &copy;</span>
+	<div class="pieCell" id='espacioBlanco'></div>
+	<a class="pieCell" id="acerca" href="presentacion/acerca.php">Acerca de nosotros</a>
 </div>
