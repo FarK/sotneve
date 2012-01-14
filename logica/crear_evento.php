d7e6fc1f76835652c1a50681e9ad6c423e28ee34
fark.zano@gmail.com <fark.zano@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-14 17:35:55 +0000

    Adaptada validación de crear_evento a la nueva clase validador

--- logica/crear_evento.php
+++ logica/crear_evento.php
@@ -2,127 +2,91 @@
 include ("test_session.php");
 include_once ("../datos/conexion.php");
 include_once ("../datos/evento.php");
-include_once ("../datos/provincia.php");
 include_once ("../datos/usuario.php");
+include_once ("validador.php");
 
 $conex = new Conexion();
-$prov = new Provincia($conex);
+$evento = new Evento($conex);
 
-$tipo = $_POST['tipos'];
-$mes = $_POST['mes'];
-$dia = $_POST['dia'];
-$ano = $_POST['ano'];
-$hora = $_POST['hora'];
-$min = $_POST['min'];
-$titulo = $_POST['nomevento'];
-$tipo = $_POST['tipos'];
-$numpersonas = $_POST['numpersonas'];
-$descripcion = $_POST['descripcion'];
-$lugar = $_POST['lugar'];
-$provincia = $_POST['provincia'];
-$actualizar = $_POST['actualizar'];
-$fechaEvento = $ano . "-" . $mes . "-" . $dia . " " . $hora . ":" . $min . ":" . "00";
+//Comprobamos que los se han pasado todos los campos
+if(!(
+	isset($_POST['nomevento'])	&&
+	isset($_POST['numpersonas'])	&&
+	isset($_POST['dia'])		&&
+	isset($_POST['mes'])		&&
+	isset($_POST['ano'])		&&
+	isset($_POST['hora'])		&&
+	isset($_POST['min'])		&&
+	isset($_POST['provincia'])	&&
+	isset($_POST['tipos'])		&&
+	isset($_POST['lugar'])		&&
+	isset($_POST['descripcion'])	&&
+	isset($_POST['actualizar'])
+)){
+	$_SESSION['error'] = 'No se han recibido los POSTs correctos';
+	header("Location: ../presentacion.errores.php");
+	exit;
+}
 
-$existeProvincia = $prov -> existeProvincia($provincia);
+//Comprobamos el formulario y actualizamos/insertamos el evento
+if (esValido($_POST)) {
+	$fechaEvento = sprintf('%s-%s-%s %s:%s:00', $_POST['ano'], $_POST['mes'], $_POST['dia'], $_POST['hora'], $_POST['min']);
 
-$valido = esValido($tipo, $existeProvincia, $provincia, $ano, $mes, $dia, $hora, $min, $titulo, $numpersonas, $provincia, $descripcion, $lugar);
+	if ($_POST['actualizar'] == -1) {
+		$evento -> insertarEvento(
+			$_POST['tipos'],
+			$fechaEvento,
+			$_POST['nomevento'],
+			$_POST['numpersonas'],
+			$_POST['provincia'],
+			$_POST['descripcion'],
+			$_POST['lugar'],
+			$_SESSION['idUsuario']);
 
-if ($valido) {
-	$evento = new Evento($conex);
-
-	if ($actualizar == -1) {
-		$evento -> insertarEvento($tipo, $fechaEvento, $titulo, $numpersonas, $provincia, $descripcion, $lugar, $_SESSION['idUsuario']);
 		$id = $conex -> getLastInsertId();
 		$evento -> inscribeUsuario($_SESSION['idUsuario'], $id);
 	} else {
-		$evento -> actualizarEvento($tipo, $fechaEvento, $titulo, $numpersonas, $provincia, $descripcion, $lugar, $_SESSION['idUsuario'], $actualizar);
-		$id = $actualizar;
+		$evento -> actualizarEvento(
+			$_POST['tipos'],
+			$fechaEvento,
+			$_POST['nomevento'],
+			$_POST['numpersonas'],
+			$_POST['provincia'],
+			$_POST['descripcion'],
+			$_POST['lugar'],
+			$_SESSION['idUsuario'],
+			$_POST['actualizar']);
+
+		//TODO: Vulnerabilidad: Creo que cualquiera puede actualizar el evento de otro a través de este post
+		$id = $_POST['actualizar'];
 	}
 
 	header(sprintf("Location:../presentacion/info_evento.php?idEvento=%s", $id));
+	exit;
 } else {
 	$_SESSION['err_campos_crearEvento'] = true;
-	header(sprintf("Location:../presentacion/crear_evento.php?idEvento=%s", $actualizar));
+	if ($_POST['actualizar'] == -1){
+		header(sprintf("Location:../presentacion/crear_evento.php", $actualizar));
+		exit;
+	}else{
+		header("Location:../presentacion/crear_evento.php");
+		exit;
+	}
 }
 
 $conex -> desconectar();
 
-function esValido($tipo, $existeProvincia, $povincia, $ano, $mes, $dia, $hora, $min, $titulo, $numpersonas, $provincia, $descripcion, $lugar) {
-	$valido = true;
-	$camposvacios = false;
 
-	if ($ano == "" || $mes == "" || $dia == "" || $hora == "" || $provincia == "" || $min == "" || $titulo == "" || $numpersonas == "" || $descripcion == "" || $lugar == "" || $provincia == "" || $tipo == "") {
-		$camposvacios = true;
-		$_SESSION['err_campos_evento'] = true;
-		$valido = false;
-		return (false);
-	}
-
-	$patronNumPersonas = "/^[[:digit:]]+$/";
-	if (!preg_match($patronNumPersonas, $numpersonas)) {
-		$valido = false;
-		return (false);
-	}
-
-	if (!$existeProvincia || $tipo < 1 || $numpersonas < 2) {
-
-		$valido = false;
-		return (false);
-
-	}
-
-	if ($min < 0 || $min > 60 || $hora < 0 || $hora > 23) {
-
-		$valido = false;
-		return (false);
-	}
-
-	/******** COMPROBACIÓN FECHA EVENTO *********/
-
-	$diamax = 0;
-	if ($mes < 1 && $mes > 12) {
-		$valido = false;
-	} else {//Comprobación de que la fecha no es posterior a la fecha actua
-		if ($ano < date("Y")) {
-			$valido = false;
-		} else if ($ano == date("Y")) {
-			if ($mes < date("m")) {
-				$valido = false;
-
-			} elseif ($mes == date("m")) {
-				if ($dia < date("d")) {
-					$valido = false;
-				}
-			}
-		}
-		switch ($mes) {
-			case '2' :
-				if ((($ano % 4 == 0) && ($ano % 100 != 0)) || (($ano % 100 == 0) && ($ano % 400 == 0))) {//Contemplo año bisiesto.
-					$diamax = 29;
-				} else {
-					$diamax = 28;
-				}
-				break;
-			case 04 || 06 || 11 || 09 :
-				$diamax = 30;
-				break;
-			case 01 || 03 || 05 || 07 || 08 || 10 || 12 :
-				$diamax = 31;
-				break;
-		}
-		if ($dia < 1 || $dia > $diamax) {
-			$valido = false;
-		}
-
-	}
-
-	/******* FIN COMPROBACIÓN FECHA EVENTO *******/
-
-	// if ($valido) {
-	// return true;
-	// } else {
-	// header("Location:../presentacion/crear_evento.php");
-	// }
-
+function esValido($_POST) {
+	return	Validador::palabras($_POST['nomevento'])	&&
+		Validador::cifra($_POST['numpersonas'])		&&
+		($_POST['numpersonas'] >= 2)			&&
+		Validador::fechaFutura($_POST['dia'],
+			$_POST['mes'],
+			$_POST['ano'])				&&
+		Validador::hora($_POST['hora'], $_POST['min'])	&&
+		Validador::idProvincia($_POST['provincia'])	&&
+		($_POST['tipos'] >= 1)
+	;
 }
 ?>
