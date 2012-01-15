5087b901959a12c5795160d105289ca129d7e7d1
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-15 12:15:39 +0000

    crear_evento.php <meta http-equiv="Content-Type" content="text/xhtml; charset=UTF-8" />

--- trunk/presentacion/crear_evento.php
+++ trunk/presentacion/crear_evento.php
@@ -268,8 +268,7 @@ function inputBoton($campos){
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
 	<head>
-		<meta content="text/xhtml; charset=UTF-8">
-		</meta>
+		<meta http-equiv="Content-Type" content="text/xhtml; charset=UTF-8" />
 		<?php tituloWeb($campos); ?>
 		<link rel="stylesheet" type="text/css" href="estilos/crear_evento.css" />
 		<script type="text/javascript" src="../logica/scripts/buscar_evento.js"></script>
