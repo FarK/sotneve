07a576593041299926ca83315171d5f31ef06afb
fark.zano@gmail.com <fark.zano@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-15 18:08:06 +0000

    Creando el acerca de

--- trunk/presentacion/acerca.php
+++ trunk/presentacion/acerca.php
@@ -9,28 +9,32 @@
 		<div id="cabecera">
 			<?php
 			session_start();
-			if (isset($_SESSION['idUsuario'])) {
+			if (isset($_SESSION['idUsuario']))
 				include ("head.php");
-			}else{
-				
-	
+			else
 				echo "<a href='../index.php'>Volver</a>";
-			}
-			
 			?>
 		</div>
 		<div>
-			<span id="nosotros"> Esta p&aacute;gina ha sido desarrollada por un grupo de 5 compa&ntilde;eros para la asignatura de ABD. Proyecto de Sotneve de ABD.
-
-	Este proyecto consiste en una aplicación web dónde los usuarios
-registrados podrán crear y/o unirse a eventos/grupos de diferentes actividades.
-
-Programadores:
-	- Alejandro Molinas Salas
-	- Jesus Vacas Bombín
-	- Rafael Espillaque Espinosa
-	- Carlos Falgueras García
-	- Antonio Rodriguez Jimenez</span>
+			<p id="nosotros">
+				Esta p&aacute;gina ha sido desarrollada por un grupo de 5 compa&ntilde;eros para la asignatura de ABD.</br>
+				Proyecto de Sotneve de ABD.</br>
+				Este proyecto consiste en una aplicaci&oacute;n web d&oacute;nde los usuarios
+ 				registrados podr&aacute;n crear y/o unirse a eventos/grupos de diferentes actividades.</br>
+				
+			</p>
+			<div class="lista">
+			<span>Programadores:</span>
+				<ul>
+					<li>Alejandro Molinas Salas</li>
+					<li>Jesus Vacas Bomb&iacute;n</li>
+					<li>Rafael Espillaque Espinosa</li>
+					<li>Carlos Falgueras Garc&iacute;a</li>
+					<li>Antonio Rodriguez Jim&eacute;</li>
+				</ul>
+			</div>
+			<div id="videoImg">
+			<img src="recursos/imagenes/esquema_relacional.png" alt="Esquema Relacional"/>
 			<video id="video" controls>
 				<source src="recursos/videos/tu_video.ogv" type="video/ogg" />
 				<source src="recursos/videos/tu_video.mp4" type="video/mp4" />
@@ -38,10 +42,96 @@ Programadores:
 					<param name="movie" value="tu_video.flv">
 					<embed src="recursos/videos/tu_video.flv" width="160" height="90">
 				</object>
-				<p>
-					Tu navegador no soporta HTML5 ni Flash Â¿En que era estÃ¡s?
-				</p>
+				<span>
+					Tu navegador no soporta HTML5 ni Flash &iquest;En que era est&aacute;s?
+				</span>
 			</video>
+			</div>
+
+			<div class="lista">
+				<span class="h2"> Estructura del c&oacute;digo de la aplicaci&oacute;n </span>
+				<p>
+				Hemos estructurado el c&oacute;digo de manera que cada capa tiene asignada una carpeta.
+				Todas las p&aacute;ginas tienen tres archivos con el mismo nombre, uno en cada carpeta de cada capa.
+				</p>
+				<dl>
+					<dt>CAPA DE PRESENTACI&Oacute;N</dt>
+					<dd>
+						<dl>
+							<dt>P&aacute;ginas de contenido</dt>
+							<dd>Todas las paginas tiene su archivo en la carpeta de presentaci&oacute;, encargado de generar la estructura xhtml</dd>
+
+							<dt>Head y footer</dt>
+							<dd>C&oacute;digo php que genera las estructuras xhtml de la cabecera y el pie de p&aacute;gina. Este c&oacute;digo se importa desde las dem&aacute;s clases</dd>
+
+							<dt>Recursos</dt>
+							<dd>Carpeta d&oacute;nde guardamos los recursos multimedia de nuestra aplicaci&oacute;n</dd>
+						</dl>
+					</dd>
+
+					<dt>CAPA DE DATOS</dt>
+					<dd>
+						<dl>
+							<dt>Clase Conexi&oacute;n</dt>
+							<dd>Se encarga de gestionar la conexi&oacute;n con la base de datos y las consultas a trav&eacute;s de la calse PDO</dd>
+							<dt>Clase Tabla</dt>
+							<dd>
+							Se encarga de pasar a un array los statament de PDO.
+							Tambi&eacute;n gestiona las consultas de campos determinados sobre una fila de una tabla espec&iacute;fica
+							</dd>
+
+							<dt>Clases hijas de Tabla</dt>
+							<dd>Objetos que agrupan las consultas que se identifican con la tabla en cuesti&oacute;n<dd>
+						</dl>
+					</dd>
+
+					<dt>CAPA DE L&Oacute;GICA</dt>
+					<dd>
+						<dl>
+							<dt>Formularios</dt>
+							<dd>Todos los formularios tiene su p&aacute;gina en la capa de l&oacute;gica que se encarga
+							de validar el formulario en el servidor</dd>
+
+							<dt>TestSesion</dt>
+							<dd>Una peque&ntilde;a porci&oacute;n de c&oacute;digo php que testea si el usuario est&aacute; o no logeado
+							y redirige al formulario de login en cado de que no lo est&eacute;</dd>
+
+							<dt>Validador</dt>
+							<dd>Clase php est&aacute;tica de apoyo para las validaciones</dd>
+
+							<dt>Clases de apoyo para AJAX</dt>
+							<dd>Generan el c&oacute;digo que va a reescribir AJAX</dd>
+						</dl>
+					<dd>
+				</dl>
+			</div>
+			<div class="lista">
+			<span class="h2"> Lista de features</span>
+			<ul>
+				<li>SEGURIDAD</li>
+				<ul>
+					<li>Validaci&oacute;n en cliente y servidor de todos los formularios</li>
+					<li>Encriptaci&oacute;n de las contrase&ntilde;as de los usuarios en SHA256</li>
+					<li>Solo el usuario propietario puede edirar su evento</li>
+					<li>Solo el usuario en editar lo relacionado con su perfil (favoritos, nombre, contrase&ntilde;a...)</li>
+				</ul>
+
+				<li>DISE&Ntilde;O y FUNCIONALIDAD</li>
+				<ul>
+					<li>Maquetaci&oacute;n intensiva y estructurada de todas las p&aacute;ginas en css</li>
+					<li>Cabecera con men&uacute; superior de navegaci&oacute;n y buscador</li>
+					<li>Buscador de eventos con 4 tipos de consultas distintas</li>
+					<li>P&aacute;gina principal con enlaces a tus favoritos, tus eventos y eventos de tu provincia</li>
+					<li>Posibilidad de crear y editar eventos</li>
+					<li>Posibilidad de editar informaci&oacute;n de tu perfil</li>
+					<li>Se puede eligir la publicaci&oacute;n o no de tu email, nombre, sexo, etc...</li>
+					<li>Elimincaci&oacute;n y agragaci&oacute;n de favoritos con AJAX</li>
+					<li>Facilidad de afiliaci&oacute;n y desafiliaci&oacute;n de eventos</li>
+					<li>Se resaltan los eventos completos</li>
+					<li>Se resaltan los campos erroreos en la validaci&oacute;n de los formularios</li>
+				</ul>
+			</ul>
+			</div>
 		</div>
 	</body>
 </html>
