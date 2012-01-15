cf484db2f14bc5b28068a9e4fe404158d5b79460
blog.de.alex@gmail.com <blog.de.alex@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-15 12:12:05 +0000

    registro.php <meta http-equiv="Content-Type" content="text/xhtml; charset=UTF-8" />

--- trunk/presentacion/registro.php
+++ trunk/presentacion/registro.php
@@ -71,7 +71,7 @@ function selectProvincias($provincias){
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
 	<head>
-		<meta content="text/xhtml; charset="utf-8" >
+		<meta http-equiv="Content-Type" content="text/xhtml; charset=UTF-8" />
 		<title>sotneve - &Uacute;nete</title>
 		<link rel="stylesheet" type="text/css" href="estilos/registro.css" />
 		<script type="text/javascript" src="../logica/scripts/registro.js"></script>
