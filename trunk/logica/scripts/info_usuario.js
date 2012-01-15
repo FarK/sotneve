b1ca93a93052ff4ce0db7f9a1b6959aa91a68d81
ervaka@gmail.com <ervaka@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-15 10:34:56 +0000

    Cambios menores

--- trunk/logica/scripts/info_usuario.js
+++ trunk/logica/scripts/info_usuario.js
@@ -14,7 +14,7 @@ var idFavorito;
 
 function insertaFavorito(idFav) {
 	idFavorito = idFav;
-	document.getElementById('add_to_favs').innerHTML = '<img id="add" src="recursos/imagenes/spinner.gif">Espere por favor';
+	document.getElementById('add_to_favs').innerHTML = '<img class="add_image" src="recursos/imagenes/spinner.gif"/> Espere por favor';
 	ajax.open('get', '../logica/inserta_favorito.php?idFav=' +idFavorito);
 	ajax.onreadystatechange = insertaCallback;
 	ajax.send(null);
@@ -22,13 +22,13 @@ function insertaFavorito(idFav) {
 function insertaCallback() {
 	if(ajax.readyState == 4){ 
 		var response = ajax.responseText;
-		document.getElementById('add_to_favs').innerHTML = '<input type="image" id="add" src="recursos/imagenes/delete.png">Borrar de favoritos';
+		document.getElementById('add_to_favs').innerHTML = '<input type="image" class="add_image" src="recursos/imagenes/delete.png"/> Borrar de favoritos';
 		document.getElementById('add_form').action = 'javascript:borraFavorito('+idFavorito+')';
 	}
 }
 function borraFavorito(idFav) {
 	idFavorito = idFav;
-	document.getElementById('add_to_favs').innerHTML = '<img id="add" src="recursos/imagenes/spinner.gif">Espere por favor';
+	document.getElementById('add_to_favs').innerHTML = '<img class="add_image" src="recursos/imagenes/spinner.gif"/> Espere por favor';
 	ajax.open('get', '../logica/borra_favorito.php?idFav=' +idFavorito);
 	ajax.onreadystatechange = borraCallback;
 	ajax.send(null);
@@ -36,7 +36,7 @@ function borraFavorito(idFav) {
 function borraCallback() {
 	if(ajax.readyState == 4){ 
 		var response = ajax.responseText;
-		document.getElementById('add_to_favs').innerHTML = '<input type="image" id="add" src="recursos/imagenes/add.png">A&ntilde;adir a favoritos';
+		document.getElementById('add_to_favs').innerHTML = '<input type="image" class="add_image" src="recursos/imagenes/add.png"/>A&ntilde;adir a favoritos';
 		document.getElementById('add_form').action = 'javascript:insertaFavorito('+idFavorito+')';
 	}
 }
