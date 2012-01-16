56c3baa79459a73457b7af20a10591a7b60c5513
ervaka@gmail.com <ervaka@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-16 23:34:57 +0000

    Añadidas stickers de validacion en el footer.php.
    Ahora general.css está validado, aunque hemos tenido que prescindir de ciertas características. Por ello, están en general2.css.

--- trunk/presentacion/footer.php
+++ trunk/presentacion/footer.php
@@ -5,6 +5,19 @@ $rutaAcerca = '/' . $ruta[1] . '/presentacion/acerca.php';
 
 <div id="pie">
 	<span class="pieCell">Copyright Sotneve 2011 &copy;</span>
+	
 	<div class="pieCell" id='espacioBlanco'></div>
-	<a class="pieCell" id="acerca" href="<?php echo $rutaAcerca; ?>">Acerca de nosotros</a>
+	
+	<img
+	class="valid"
+	src="http://www.w3.org/Icons/valid-xhtml10" 
+	alt="Valid XHTML 1.0 Strict" 
+	height="31" width="88" /></a>
+
+	<img style="border:0;width:88px;height:31px"
+	class="valid"
+	src="http://jigsaw.w3.org/css-validator/images/vcss-blue"
+	alt="¡CSS Válido!" />
+	
+	<a class="pieCell" id="acerca" href="<?php echo $rutaAcerca;?>">Acerca de nosotros</a>
 </div>
