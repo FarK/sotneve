76943c259bbf67d5b95458d0255bf0fcd7ef6286
rafaespillaque@gmail.com <rafaespillaque@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-16 09:19:03 +0000

    info evento e info usuario validados

--- logica/scripts/info_evento.js
+++ logica/scripts/info_evento.js
@@ -22,7 +22,7 @@ function inscribeEvento(idEv) {
 function inscribeCallback() {
 	if(ajax.readyState == 4){ 
 		var response = ajax.responseText;
-		document.getElementById('me_apunto').innerHTML = '<input type="image" id="add" src="recursos/imagenes/delete.png">Desinscribirse';
+		document.getElementById('me_apunto').innerHTML = '<input type="image" id="add" src="recursos/imagenes/delete.png" />Desinscribirse';
 		document.getElementById('informacion').action = 'javascript:desinscribeEvento('+idEvento+')';
 		if(response == "")
 			document.getElementById('num_asistentes').innerHTML = "Ninguna persona asiste</br>";
@@ -33,7 +33,7 @@ function inscribeCallback() {
 }
 function desinscribeEvento(idEv) {
 	idEvento = idEv;
-	document.getElementById('me_apunto').innerHTML = '<img id="add" src="recursos/imagenes/spinner.gif">Espere por favor';
+	document.getElementById('me_apunto').innerHTML = '<img id="add" src="recursos/imagenes/spinner.gif" />Espere por favor';
 	ajax.open('get', '../logica/desinscribe_evento.php?idEv=' +idEvento);
 	ajax.onreadystatechange = desinscribeCallback;
 	ajax.send(null);
