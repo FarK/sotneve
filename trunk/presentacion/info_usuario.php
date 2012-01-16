7a94895bfb8d8c40b7e650727e20d9444e3a14cf
fark.zano@gmail.com <fark.zano@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-16 10:55:43 +0000

    Validado info_usuario

--- trunk/presentacion/info_usuario.php
+++ trunk/presentacion/info_usuario.php
@@ -53,134 +53,136 @@
 		include ("head.php");
 		?>
 
-		<div class='lista_usuarios' id="amigos">
-			<p>
-				<?php 
-				$titulo = sprintf('<strong class="num_usuarios">Favoritos de %s:</strong>', $camposUsuario['alias']);
-				
-				echo $titulo;
-				?>
-				<br />
+		<div class="contenido">
+			<div class='lista_usuarios' id="amigos">
+				<p>
+					<?php 
+					$titulo = sprintf('<strong class="num_usuarios">Favoritos de %s:</strong>', $camposUsuario['alias']);
+					
+					echo $titulo;
+					?>
+					<br />
+					<?php
+
+					//Comprobamos si tiene o no favoritos
+					if(empty($favoritos ))
+						echo '<span> Actualmente no hay favoritos </span>';
+
+
+					foreach($favoritos as $fav){
+						$span= sprintf("<a class='enlaceEnmarcado' href='info_usuario.php?idUsuario=%s'>%s</a>\n\t\t", $fav['idUsuario2'],$fav['alias']);
+						echo $span;
+					}
+					?>
+				</p>
+			</div>
+			<div id="acciones_usuario">
+				<form id="add_form" method="post" action='<?php echo $add_form_action ?>'>
+					<div id='add_to_favs'>
+						<?php echo $add_image ?>
+					</div>
+				</form>
+			
 				<?php
+				$form = sprintf('
+					<form method="post" id="rate_form" action="javascript:valoraUsuario(%s)">
+					<div id="rate_user">
+						<input type="image" class="add_image" src="recursos/imagenes/valoracion.png" />
+						<span id="valora" class="texto_accion">&iexcl;Punt&uacute;alo!</span>
+					</div>
+					</form>', $idUserVisitado);
+				echo $form;?>
+			</div>
 
-				//Comprobamos si tiene o no favoritos
-				if(empty($favoritos ))
-					echo '<span> Actualmente no hay favoritos </span>';
-
-
-				foreach($favoritos as $fav){
-					$span= sprintf("<a class='enlaceEnmarcado' href='info_usuario.php?idUsuario=%s'>%s</a>\n\t\t", $fav['idUsuario2'],$fav['alias']);
-					echo $span;
-				}
-				?>
-			</p>
-		</div>
-		<div id="acciones_usuario">
-			<form id="add_form" method="post" action='<?php echo $add_form_action ?>'>
-				<div id='add_to_favs'>
-					<?php echo $add_image ?>
-				</div>
-			</form>
-		
+			<span class="h1"><?php echo $camposUsuario['alias']
+			?></span>
 			<?php
-			$form = sprintf('
-				<form method="post" id="rate_form" action="javascript:valoraUsuario(%s)">
-				<div id="rate_user">
-					<input type="image" class="add_image" src="recursos/imagenes/valoracion.png" />
-					<span id="valora" class="texto_accion">&iexcl;Punt&uacute;alo!</span>
-				</div>
-				</form>', $idUserVisitado);
-			echo $form;?>
-		</div>
 
-		<span class="h1"><?php echo $camposUsuario['alias']
-		?></span>
-		<?php
+			$novisible = "No Disponible";
+			$nombre = $novisible;
+			$apellidos = $novisible;
+			$sexo = $novisible;
+			$fechaNac = $novisible;
+			$email = $novisible;
+			$provincia = $novisible;
 
-		$novisible = "No Disponible";
-		$nombre = $novisible;
-		$apellidos = $novisible;
-		$sexo = $novisible;
-		$fechaNac = $novisible;
-		$email = $novisible;
-		$provincia = $novisible;
+			if ($camposUsuario['visibilidad'] & $NOMBRE) {
+				$nombre = $camposUsuario['nombre'];
 
-		if ($camposUsuario['visibilidad'] & $NOMBRE) {
-			$nombre = $camposUsuario['nombre'];
-
-		}
-
-		if ($camposUsuario['visibilidad'] & $APELLIDOS) {
-			$apellidos = $camposUsuario['apellidos'];
-
-		}
-
-		if ($camposUsuario['visibilidad'] & $SEXO) {
-			$sexo = $camposUsuario['sexo'];
-
-			if ($sexo == 1) {
-				$sexo = "Hombre";
-			} elseif ($sexo == 0) {
-				$sexo = "Mujer";
 			}
-		}
 
-		if ($camposUsuario['visibilidad'] & $FECHA_NAC) {
-			$fechaNac = $camposUsuario['fechaNac'];
-		}
+			if ($camposUsuario['visibilidad'] & $APELLIDOS) {
+				$apellidos = $camposUsuario['apellidos'];
 
-		if ($camposUsuario['visibilidad'] & $EMAIL) {
-			$email = $camposUsuario['email'];
-		}
-		
-		if ($camposUsuario['visibilidad'] & $PROVINCIA) {
-			$provincia = $provActual['nombre'];
-		}
-		
-		$alias = $camposUsuario['alias'];
-		?>
-		<div class='info_usuario'>
-			<div class='data_row'>
-				<span class="l_usuario"> Nombre: </span>
-				<span class="c_usuario"> <?php
-				echo $nombre;
-					?></span>
-			</div>
-			<div class='data_row'>
-				<span class="l_usuario"> Apellidos: </span>
-				<span class="c_usuario"> <?php
-				echo $apellidos;
-					?></span>
-			</div>
-			<div class='data_row'>
-				<span class="l_usuario"> Sexo: </span>
-				<span class="c_usuario"> <?php
-				echo $sexo;
-					?></span>
-			</div>
-			<div class='data_row'>
-				<span class="l_usuario"> Fecha de Nacimiento: </span>
-				<span class="c_usuario"> <?php
-				echo $fechaNac;
-					?></span>
-			</div>
-			<div class='data_row'>
-				<span class="l_usuario"> Email: </span>
-				<span class="c_usuario"> <?php
-				echo $email;
-					?></span>
-			</div>
-			<div class='data_row'>
-				<span class="l_usuario"> Provincia: </span>
-				<span class="c_usuario"> <?php
-				echo $provincia;
-					?></span>
-			</div>
-			<div class='data_row'>
-				<span class="l_usuario"> Valoraci&oacute;n: </span>
-				<span id='val' class="c_usuario"> <?php
-				echo number_format($valoracion, 2, ',', '.');
-					?></span>
+			}
+
+			if ($camposUsuario['visibilidad'] & $SEXO) {
+				$sexo = $camposUsuario['sexo'];
+
+				if ($sexo == 1) {
+					$sexo = "Hombre";
+				} elseif ($sexo == 0) {
+					$sexo = "Mujer";
+				}
+			}
+
+			if ($camposUsuario['visibilidad'] & $FECHA_NAC) {
+				$fechaNac = $camposUsuario['fechaNac'];
+			}
+
+			if ($camposUsuario['visibilidad'] & $EMAIL) {
+				$email = $camposUsuario['email'];
+			}
+			
+			if ($camposUsuario['visibilidad'] & $PROVINCIA) {
+				$provincia = $provActual['nombre'];
+			}
+			
+			$alias = $camposUsuario['alias'];
+			?>
+			<div class='info_usuario'>
+				<div class='data_row'>
+					<span class="l_usuario"> Nombre: </span>
+					<span class="c_usuario"> <?php
+					echo $nombre;
+						?></span>
+				</div>
+				<div class='data_row'>
+					<span class="l_usuario"> Apellidos: </span>
+					<span class="c_usuario"> <?php
+					echo $apellidos;
+						?></span>
+				</div>
+				<div class='data_row'>
+					<span class="l_usuario"> Sexo: </span>
+					<span class="c_usuario"> <?php
+					echo $sexo;
+						?></span>
+				</div>
+				<div class='data_row'>
+					<span class="l_usuario"> Fecha de Nacimiento: </span>
+					<span class="c_usuario"> <?php
+					echo $fechaNac;
+						?></span>
+				</div>
+				<div class='data_row'>
+					<span class="l_usuario"> Email: </span>
+					<span class="c_usuario"> <?php
+					echo $email;
+						?></span>
+				</div>
+				<div class='data_row'>
+					<span class="l_usuario"> Provincia: </span>
+					<span class="c_usuario"> <?php
+					echo $provincia;
+						?></span>
+				</div>
+				<div class='data_row'>
+					<span class="l_usuario"> Valoraci&oacute;n: </span>
+					<span id='val' class="c_usuario"> <?php
+					echo number_format($valoracion, 2, ',', '.');
+						?></span>
+				</div>
 			</div>
 		</div>
 		<?php
