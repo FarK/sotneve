2c78c11ae08603a8d76764ad2b08564430380cd5
antrodjim <antrodjim@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-17 08:39:42 +0000

    Arreglado error del campo puntuacion a la hora de que un usuario estuviese sin puntuar.

--- trunk/presentacion/info_usuario.php
+++ trunk/presentacion/info_usuario.php
@@ -1,40 +1,40 @@
 <?php
-	include ("../logica/test_session.php");
-	include_once('../datos/conexion.php');
-	include_once('../datos/usuario.php');
-	include_once('../datos/favorito.php');
-	
-	if(!isset($_GET["idUsuario"])){
-		$_SESSION['error'] = "userNotFound";
-		header("Location:errores.php");
-		exit;
-	}
-	
-	$conex = new Conexion();
-	//Se llama usuario visitado porque si no se pisa con usuario de head.php
-	$idUserVisitado = $_GET["idUsuario"];
-	$usuarioVisitado = new Usuario($conex, $idUserVisitado);
-	$favorito = new Favorito($conex);
-	$userLog = new Usuario($conex, $_SESSION['idUsuario']);
-	//Consultamos los campos del usuario
-	$camposUsuario = $usuarioVisitado->consultarTodosLosCampos();
-	$provincia = $usuarioVisitado -> getProvincia();
-	$provActual = $provincia;
-	
-	$valoracion = $usuarioVisitado->getValoracion();
-	if($valoracion == -1)
+include ("../logica/test_session.php");
+include_once ('../datos/conexion.php');
+include_once ('../datos/usuario.php');
+include_once ('../datos/favorito.php');
+
+if (!isset($_GET["idUsuario"])) {
+	$_SESSION['error'] = "userNotFound";
+	header("Location:errores.php");
+	exit ;
+}
+
+$conex = new Conexion();
+//Se llama usuario visitado porque si no se pisa con usuario de head.php
+$idUserVisitado = $_GET["idUsuario"];
+$usuarioVisitado = new Usuario($conex, $idUserVisitado);
+$favorito = new Favorito($conex);
+$userLog = new Usuario($conex, $_SESSION['idUsuario']);
+//Consultamos los campos del usuario
+$camposUsuario = $usuarioVisitado -> consultarTodosLosCampos();
+$provincia = $usuarioVisitado -> getProvincia();
+$provActual = $provincia;
+
+$valoracion = $usuarioVisitado -> getValoracion();
+if ($valoracion == -1)
 	$valoracion = "Sin datos";
-	
-	//Consultamos los favoritos del usuario
-	$favoritos= $favorito->getFavoritos($_GET['idUsuario']);
-	$esFavorito = $userLog->esFavorito($idUserVisitado);
-	if($esFavorito){
-		$add_form_action = 'javascript:borraFavorito('.$idUserVisitado.')';
-		$add_image = '<input type="image" class="add_image" src="recursos/imagenes/delete.png"/><span class="texto_accion"> Borrar de favoritos</span>';
-	}else{
-		$add_form_action = 'javascript:insertaFavorito('.$idUserVisitado.')';
-		$add_image = '<input type="image" class="add_image" src="recursos/imagenes/add.png"/><span class="texto_accion"> A&ntilde;adir a favoritos</span>';
-	}
+
+//Consultamos los favoritos del usuario
+$favoritos = $favorito -> getFavoritos($_GET['idUsuario']);
+$esFavorito = $userLog -> esFavorito($idUserVisitado);
+if ($esFavorito) {
+	$add_form_action = 'javascript:borraFavorito(' . $idUserVisitado . ')';
+	$add_image = '<input type="image" class="add_image" src="recursos/imagenes/delete.png"/><span class="texto_accion"> Borrar de favoritos</span>';
+} else {
+	$add_form_action = 'javascript:insertaFavorito(' . $idUserVisitado . ')';
+	$add_image = '<input type="image" class="add_image" src="recursos/imagenes/add.png"/><span class="texto_accion"> A&ntilde;adir a favoritos</span>';
+}
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es" lang="es">
@@ -42,7 +42,6 @@
 		<!-- IMPORTANTE ESA LÍNEA DE ABAJO!!!  -->
 		<title>Sotneve - <?php echo $camposUsuario['alias'];?></title>
 		<meta http-equiv="Content-Type" content="text/xhtml; charset=UTF-8" />
-		
 		<link rel="stylesheet" type="text/css" href="estilos/info_usuario.css"/>
 		<script type="text/javascript" src="../logica/scripts/buscarevento.js"></script>
 		<script type="text/javascript" src="../logica/scripts/info_usuario.js"></script>
@@ -56,21 +55,20 @@
 		<div class="contenido">
 			<div class='lista_usuarios' id="amigos">
 				<p>
-					<?php 
+					<?php
 					$titulo = sprintf('<strong class="num_usuarios">Favoritos de %s:</strong>', $camposUsuario['alias']);
-					
+
 					echo $titulo;
 					?>
 					<br />
 					<?php
 
 					//Comprobamos si tiene o no favoritos
-					if(empty($favoritos ))
+					if (empty($favoritos))
 						echo '<span> Actualmente no hay favoritos </span>';
 
-
-					foreach($favoritos as $fav){
-						$span= sprintf("<a class='enlaceEnmarcado' href='info_usuario.php?idUsuario=%s'>%s</a>\n\t\t", $fav['idUsuario2'],$fav['alias']);
+					foreach ($favoritos as $fav) {
+						$span = sprintf("<a class='enlaceEnmarcado' href='info_usuario.php?idUsuario=%s'>%s</a>\n\t\t", $fav['idUsuario2'], $fav['alias']);
 						echo $span;
 					}
 					?>
@@ -79,23 +77,23 @@
 			<div id="acciones_usuario">
 				<form id="add_form" method="post" action='<?php echo $add_form_action ?>'>
 					<div id='add_to_favs'>
-						<?php echo $add_image ?>
+						<?php echo $add_image
+						?>
 					</div>
 				</form>
-			
 				<?php
 				$form = sprintf('
-					<form method="post" id="rate_form" action="javascript:valoraUsuario(%s)">
-					<div id="rate_user">
-						<input type="image" class="add_image" src="recursos/imagenes/valoracion.png" />
-						<span id="valora" class="texto_accion">&iexcl;Punt&uacute;alo!</span>
-					</div>
-					</form>', $idUserVisitado);
-				echo $form;?>
+<form method="post" id="rate_form" action="javascript:valoraUsuario(%s)">
+<div id="rate_user">
+<input type="image" class="add_image" src="recursos/imagenes/valoracion.png" />
+<span id="valora" class="texto_accion">&iexcl;Punt&uacute;alo!</span>
+</div>
+</form>', $idUserVisitado);
+				echo $form;
+				?>
 			</div>
-
 			<span class="h1"><?php echo $camposUsuario['alias']
-			?></span>
+				?></span>
 			<?php
 
 			$novisible = "No Disponible";
@@ -133,11 +131,11 @@
 			if ($camposUsuario['visibilidad'] & $EMAIL) {
 				$email = $camposUsuario['email'];
 			}
-			
+
 			if ($camposUsuario['visibilidad'] & $PROVINCIA) {
 				$provincia = $provActual['nombre'];
 			}
-			
+
 			$alias = $camposUsuario['alias'];
 			?>
 			<div class='info_usuario'>
@@ -180,7 +178,11 @@
 				<div class='data_row'>
 					<span class="l_usuario"> Valoraci&oacute;n: </span>
 					<span id='val' class="c_usuario"> <?php
-					echo number_format($valoracion, 2, ',', '.');
+					if (!is_numeric($valoracion)) {
+						echo("Sin puntuar");
+					} else {
+						echo number_format($valoracion, 2, '.', ',');
+					}
 						?></span>
 				</div>
 			</div>
