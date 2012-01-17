71314010d8ab25dbd6d87624f2c81846ce875f95
fark.zano@gmail.com <fark.zano@gmail.com@6b5a4d6b-188b-c7b3-9211-eada34284ce5>
2012-01-17 06:25:01 +0000

    Terminada la página! :D

--- trunk/presentacion/acerca.php
+++ trunk/presentacion/acerca.php
@@ -13,7 +13,7 @@
 			if (isset($_SESSION['idUsuario']))
 				include ("head.php");
 			else
-				echo "<a href='../index.php'>Volver</a>";
+				echo "<a id='enlaceLogo' href='../index.php'><img id='logo' src='recursos/imagenes/logoHead.png' alt='Inicio'></img></a>"
 			?>
 		</div>
 		<div>
@@ -25,20 +25,49 @@
 			</p>
 			<div class="lista">
 			<span class="h2">Programadores</span>
-				<ul>
+				<ul id="programadores">
 					<li>Alejandro Molinas Salas</li>
 					<li>Jesus Vacas Bomb&iacute;n</li>
 					<li>Rafael Espillaque Espinosa</li>
 					<li>Carlos Falgueras Garc&iacute;a</li>
 					<li>Antonio Rodriguez Jim&eacute;nez</li>
 				</ul>
+				<img id="desarrolladores" src="recursos/imagenes/desarrolladores.jpg" alt="Programadores"/>
 			</div>
-			<div id="videoImg">
-			<img src="recursos/imagenes/esquema_relacional.png" alt="Esquema Relacional"/>
-			<video controls="">
-				<source type="video/webm" src="recursos/videos/tu_video.webm"></source>
-			</video>
+
+			<br/>
+
+			<div class="lista">
+				<span class="h2"> Lista de features</span>
+				<ul>
+					<li>SEGURIDAD
+						<ul>
+							<li>Validaci&oacute;n en cliente y servidor de todos los formularios</li>
+							<li>Encriptaci&oacute;n de las contrase&ntilde;as de los usuarios en SHA256</li>
+							<li>Solo el usuario propietario puede edirar su evento</li>
+							<li>Solo el usuario en editar lo relacionado con su perfil (favoritos, nombre, contrase&ntilde;a...)</li>
+						</ul>
+					</li>
+
+					<li>DISE&Ntilde;O y FUNCIONALIDAD
+						<ul>
+							<li>Maquetaci&oacute;n intensiva y estructurada de todas las p&aacute;ginas en css</li>
+							<li>Cabecera con men&uacute; superior de navegaci&oacute;n y buscador</li>
+							<li>Buscador de eventos con 4 tipos de consultas distintas</li>
+							<li>P&aacute;gina principal con enlaces a tus favoritos, tus eventos y eventos de tu provincia</li>
+							<li>Posibilidad de crear y editar eventos</li>
+							<li>Posibilidad de editar informaci&oacute;n de tu perfil</li>
+							<li>Se puede eligir la publicaci&oacute;n o no de tu email, nombre, sexo, etc...</li>
+							<li>Elimincaci&oacute;n y agragaci&oacute;n de favoritos con AJAX</li>
+							<li>Facilidad de afiliaci&oacute;n y desafiliaci&oacute;n de eventos</li>
+							<li>Se resaltan los eventos completos</li>
+							<li>Se resaltan los campos erroreos en la validaci&oacute;n de los formularios</li>
+						</ul>
+					</li>
+				</ul>
 			</div>
+
+			<br/>
 
 			<div class="lista">
 				<span class="h2"> Estructura del c&oacute;digo de la aplicaci&oacute;n </span>
@@ -97,76 +126,60 @@
 					</dd>
 				</dl>
 			</div>
+
+			<br/>
+
 			<div class="lista">
-				<span class="h2"> Lista de features</span>
+				<span class="h2"> Lista de puntos de control conseguidos</span>
 				<ul>
-					<li>SEGURIDAD
+					<li>Evaluación Basica
 						<ul>
-							<li>Validaci&oacute;n en cliente y servidor de todos los formularios</li>
-							<li>Encriptaci&oacute;n de las contrase&ntilde;as de los usuarios en SHA256</li>
-							<li>Solo el usuario propietario puede edirar su evento</li>
-							<li>Solo el usuario en editar lo relacionado con su perfil (favoritos, nombre, contrase&ntilde;a...)</li>
+							<li>Entrega correcta de la pr&aacute;ctica</li>
+							<li>Instalación correcta (index.html/php)</li>
+							<li>Ausencia de errores de programaci&oacute;n</li>
+							<li>Uso marcado HTML/XHTML estricto</li>
+							<li>Uso de CSS en archivos externos</li>
+							<li>Maquetacion CSS en todas las p&aacute;ginas</li>
+							<li>Formularios</li>
+							<li>Validacion en cliente de todos los formularios</li>
+							<li>Validaci&oacute;n en servidor de todos los formularios</li>
+							<li>Base de datos (3FN)</li>
+							<li>Inserci&oacute;n en BD</li>
+							<li>Actualizaci&oacute;n en la BD</li>
+							<li>Borrado en la BD</li>
+							<li>Consulta a BD formateando resultados en tabla HTML</li>
+							<li>Tratamiento de errores de acceso a BD en servidor</li>
 						</ul>
 					</li>
-
-					<li>DISE&Ntilde;O y FUNCIONALIDAD
+					<li>Evaluacion Avanzada
 						<ul>
-							<li>Maquetaci&oacute;n intensiva y estructurada de todas las p&aacute;ginas en css</li>
-							<li>Cabecera con men&uacute; superior de navegaci&oacute;n y buscador</li>
-							<li>Buscador de eventos con 4 tipos de consultas distintas</li>
-							<li>P&aacute;gina principal con enlaces a tus favoritos, tus eventos y eventos de tu provincia</li>
-							<li>Posibilidad de crear y editar eventos</li>
-							<li>Posibilidad de editar informaci&oacute;n de tu perfil</li>
-							<li>Se puede eligir la publicaci&oacute;n o no de tu email, nombre, sexo, etc...</li>
-							<li>Elimincaci&oacute;n y agragaci&oacute;n de favoritos con AJAX</li>
-							<li>Facilidad de afiliaci&oacute;n y desafiliaci&oacute;n de eventos</li>
-							<li>Se resaltan los eventos completos</li>
-							<li>Se resaltan los campos erroreos en la validaci&oacute;n de los formularios</li>
+							<li>P&aacute;gina de descripción de la aplicaci&oacute;n en HTML</li>
+							<li>Uso avanzado de JavaScript</li>
+							<li>Uso de expresiones regulares en JavaScript o PHP</li>
+							<li>Facilidad de navegaci&oacute;n</li>
+							<li>Usabilidad de la aplicacion</li>
+							<li>Modularidad del c&oacute;digo en cliente</li>
+							<li>Modularidad del c&oacute;digo en servidor</li>
+							<li>Complejidad de la base de datos</li>
+							<li>Integridad referencial</li>
+							<li>Uso de sesi&oacute;n en PHP</li>
+							<li>Legibilidad del c&oacute;digo</li>
+							<li>HTML validado por W3C</li>
+							<li>CSS validado por W3C</li>
+							<li>Otros conocimientos</li>
 						</ul>
 					</li>
 				</ul>
 			</div>
-			<div class="lista">
-				<span class="h2"> Lista de control de trabajo en grupo </span>
-				<ul>
-					<li>Evaluación Basica
-						<ul>
-							<li>Entrega correcta de la práctica </li>
-							<li>Instalación correcta (index.html/php) </li>
-							<li>Ausencia de errores de programación</li>
-							<li>Uso marcado HTML/XHTML estricto. Si, en cada página de presentacion.</li>
-							<li>Uso de CSS en archivos externos. Si.</li>
-							<li>Maquetacion CSS en todas las paginas. Si.</li>
-							<li>Formularios. Si, registro, crear_evento...</li>
-							<li>Validacion en cliente de todos los formularios. Si, usando javascript</li>
-							<li>Validacion en servidor de todos los formularios. Si, en la capa de logica</li>
-							<li>Base de datos (3FN). Si</li>
-							<li>Inserción en BD. Si, al registrar un usuario por ejemplo.</li>
-							<li>Actualizacion en la BD, si al cambiar los datos personales.</li>
-							<li>Borrado en la BD. Si, al eliminar un favorito por ejemplo.</li>
-							<li>Consulta a BD formateando resultados en tabla HTML. Si, en los resultados de la busqueda.</li>
-							<li>Tratamiento de errores de acceso a BD en servidor. Si, haciendo uso del objeto conexion que gestiona dichos errores</li>
-						</ul>
-					</li>
-										<li>Evaluacion Avanzada
-						<ul>
-							<li>Página de descripción de la aplicacion en HTML. Si, esta misma.</li>
-							<li>Uso avanzado de JavaScript. Si.</li>
-							<li>Uso de expresiones regulares en JavaScript o PHP. Si, cuando validamos formularios. ej: registro</li>
-							<li>Facilidad de navegacion. Si</li>
-							<li>Usabilidad de la aplicacion. Si</li>
-							<li>Modularidad del codigo en cliente. ?????????</li>
-							<li>Modularidad del codigo en servidor. Si, muy modular.</li>
-							<li>Complejidad de la base de datos. Si, 7 tablas relacionadas</li>
-							<li>Integridad referencial. ???????</li>
-							<li>Uso de sesion en PHP. Si, para el login, y transmision de variables entre paginas</li>
-							<li>Legibilidad del codigo. Si</li>
-							<li>HTML validado por W3C. Si</li>
-							<li>CSS validado por W3C.Todavia no</li>
-							<li>Otros conocimientos. Si, ajax para añadir favoritos y inscribirse en eventos. HTML5 para el video</li>
-						</ul>
-					</li>
-				</ul>
+
+			<div id="videoImg">
+			<img src="recursos/imagenes/esquema_relacional.png" alt="Esquema Relacional"/>
+			<img src="recursos/imagenes/loc.png" alt="Activity day"/>
+			<img src="recursos/imagenes/activity_day.png" alt="Activity day"/>
+			<img src="recursos/imagenes/activity_time.png" alt="Activity time"/>
+			<video controls="">
+				<source type="video/webm" src="recursos/videos/gource.webm"></source>
+			</video>
 			</div>
 		</div>
 		<?php include ('footer.php'); ?>
